10944e342e4a9aed8e4f8038b9cfc83a85da2d5c
Guillermo Royo <guillermo.royo.rivera@gmail.com>
2024-08-30 22:41:54 -0400

    add celery flower and beat scheduler

--- investsphere_api/celery.py
+++ investsphere_api/celery.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
+from celery.schedules import crontab
 
 # os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'investsphere_api.settings.prod')
 
@@ -9,3 +10,11 @@ app = Celery('investsphere_api')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
 app.autodiscover_tasks()
+
+app.conf.beat_schedule = {
+    'test_task': {
+        'task': 'app_users.tasks.test_task',
+        'schedule': crontab(minute='*/5'),   
+        'args': (), 
+    },
+}

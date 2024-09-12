1f8a8fb6d44e816d418911f7ccbea6d4d8ce8fe4
Guillermo Royo <guillermo.royo.rivera@gmail.com>
2024-09-12 20:55:59 -0300

    add permissions

--- app_transactions/viewset.py
+++ app_transactions/viewset.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-
 # Create your views here.
+from rest_framework.permissions import IsAuthenticated
 from rest_framework import viewsets
 from .models import CryptocurrencyTransaction
 from .serializers import CryptocurrencyTransactionSerializer
@@ -8,6 +8,7 @@ from .serializers import CryptocurrencyTransactionSerializer
 class CryptocurrencyTransactionViewSet(viewsets.ModelViewSet):
     queryset = CryptocurrencyTransaction.objects.all()
     serializer_class = CryptocurrencyTransactionSerializer
+    permission_classes = [IsAuthenticated]
 
     def perform_create(self, serializer):
         # Si deseas personalizar alguna lógica en el proceso de creación
